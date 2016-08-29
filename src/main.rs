751e0b0f6ff28d07e325a9805a70fb7faf6870ff
Kevin_Pellet_(Ilphrin) <ilphrin@autistici.org>
2016-08-30 01:39:44 +0200

    Now user can save a feed, and see the list of saved feed

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,53 @@ extern crate rss;
 extern crate url;
 extern crate curl;
 extern crate chrono;
+extern crate glob;
 
-mod save;
+mod streamrss;
 
-use std::str;
 use std::io;
-
+use std::io::prelude::*;
+use glob::glob;
+use std::path::Path;
+use std::fs::File;
 
 fn main() {
   let mut buffer = String::new();
 
-  io::stdin().read_line(&mut buffer).ok().expect("Failed to read line");
-  buffer = String::from(buffer.trim_right());
-  let rss_feed = save::StreamRSS::new(buffer);
-  for elem in rss_feed.items {
-    println!("{}", elem.title.unwrap());
+  loop {
+    println!("What do you want to do?");
+    println!("[1] List currently saved feeds");
+    println!("[2] Save a new feed");
+    println!("[3] Remove a feed");
+    println!("[4] Leave");
+    println!("");
+
+    io::stdin().read_line(&mut buffer).ok().expect("Failed to read line");
+    buffer = String::from(buffer.trim_right());
+    match buffer.as_str() {
+      "1" => {
+        println!("==========");
+        let files = glob("./feeds/*").unwrap().filter_map(std::result::Result::ok);
+        for file in files {
+          let opened_file = File::open(file).unwrap();
+          let line = io::BufReader::new(opened_file).lines().filter_map(std::result::Result::ok).collect::<Vec<String>>();
+          println!("{}", line[0]);
+
+
+        }
+        println!("==========");
+      }
+      "2" => {
+        let mut name = String::new();
+        io::stdin().read_line(&mut name).ok().expect("Failed to read line");
+        name = String::from(name.trim_right());
+        let rss_feed = streamrss::StreamRSS::new(&name);
+        streamrss::save_feed(&rss_feed);
+      }
+      "3" => println!("REMOVING"),
+      "4" => break,
+      _ => println!("WRONG ANSWER JACK")
+    }
+    buffer.clear();
   }
-  println!("{}", rss_feed.name);
 }
---
+++ src/streamrss/mod.rs
@@ -0,0 +1,118 @@
+use std::str;
+
+use rss::*;
+use chrono::*;
+use url::{Url};
+use curl::easy::Easy;
+
+use std::io::prelude::*;
+use std::io;
+use std::fs::File;
+use std::path::Path;
+
+
+struct FeedGetter<'data> {
+  feed: &'data mut String
+}
+
+pub fn get_feed(buffer: &String) -> Channel {
+  let mut feed_str = String::new();
+
+  {
+    let getter = FeedGetter{feed: &mut feed_str};
+
+    match Url::parse(buffer.as_str()) {
+        Ok(_) => {
+          let mut easy = Easy::new();
+          easy.url(buffer.as_str()).unwrap();
+          let mut transfer = easy.transfer();
+          transfer.write_function(|data| {
+            match str::from_utf8(data) {
+              Ok(elem) => getter.feed.push_str(elem),
+              Err(_) => {}
+            }
+            Ok(data.len())
+          }).unwrap();
+          match transfer.perform() {
+            Ok(_) => {},
+            Err(e) => println!("Error while performing transfer: {}", e)
+          }
+        },
+        Err(e) => {
+          getter.feed.push_str("ERROR");
+          println!("Invalid address: {}", e);
+        }
+    }
+
+  }
+  match feed_str.parse::<Rss>() {
+    Ok(value) => {
+      let Rss(feed) = value;
+      return feed
+    },
+    Err(why) => {
+      panic!("{}", why);
+    }
+  }
+}
+
+pub struct StreamRSS {
+  pub url: String,
+  pub name: String,
+  pub items: Vec<Item>,
+  pub object: Channel,
+  pub last_update: DateTime<Local>,
+  // pub favico: Image,
+  // pub description: String,
+}
+
+impl StreamRSS {
+  pub fn new(url: &String) -> StreamRSS {
+    let feed = get_feed(&url);
+    StreamRSS{url: url.clone(), name: feed.title.clone(), items: feed.items.clone(), object: feed, last_update:Local::now()}
+  }
+}
+
+pub fn save_feed(feed: &StreamRSS) {
+  let mut buffer = String::new();
+  let mut path = String::new();
+  path.push_str("feeds/");
+
+  println!("Give him a name");
+  io::stdin().read_line(&mut buffer).ok().expect("Failed to read line");
+  buffer = String::from(buffer.trim_right());
+  path.push_str(buffer.as_str());
+  path.push_str(".rss");
+
+  let path = Path::new(path.as_str());
+  let display = path.display();
+  let mut file;
+
+  match File::open(&path) {
+    Err(why) => {
+      println!("{}", why);
+      file = match File::create(path) {
+        Err(_) => panic!("couldn't create {}", display),
+        Ok(file) => file
+      };
+    }
+    Ok(_) => {
+      println!("File {} already exists", display);
+      return;
+    }
+  }
+
+  let mut all_file = String::new();
+  all_file.push_str(feed.name.as_str());
+  all_file.push('\n');
+  all_file.push_str(feed.url.as_str());
+  all_file.push('\n');
+  all_file.push_str(feed.last_update.to_string().as_str());
+
+  match file.write_all(all_file.as_bytes()) {
+    Err(_) => println!("couldn't write to {}", display),
+    Ok(_) => println!("Successfully wrote to {}", display)
+  }
+  println!("");
+}
+
