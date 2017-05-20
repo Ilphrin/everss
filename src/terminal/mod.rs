38298f70c0ae821288daba1fa214df3c87596744
Kevin Ilphrin Pellet <ilphrin@autistici.org>
2017-05-20 17:40:29 +0200

    Change library and move function to terminal
    Use termion instead of term for better terminal manipulations
    Move computation from main to terminal module

--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -2,6 +2,7 @@ use std::io;
 use std::fs;
 
 use streamrss::*;
+use termion;
 
 pub struct Curses {
   pub streams: Vec<StreamRSS>,
@@ -40,21 +41,63 @@ impl Irss for Curses {
     }
   }
 
-  fn remove_feed(&mut self, index: usize) {
-    if index >= self.streams.len() {
-      println!("Too high value");
-      return;
+  fn remove_feed(&mut self) {
+    println!("Which feed do you want to remove?");
+    println!("(Enter -1 if you just want to go back and see the list)");
+    let mut value = String::new();
+    io::stdin().read_line(&mut value).ok().expect("Failed to read line");
+    value.pop();
+    match value.parse::<usize>() {
+      Ok(index) => {
+        if index >= 0 {
+            if index >= self.streams.len() {
+              println!("Too high value");
+              return;
+            }
+            let mut file = String::new();
+            file.push_str("feeds/");
+            file.push_str(self.streams[index].name.as_str());
+            file.push_str(".rss");
+            match fs::remove_file(file.as_str()) {
+              Ok(_) => {
+                println!("Removed {}", self.streams[index].name);
+                self.streams.remove(index);
+              },
+              Err(why) => println!("[ERROR] Can't remove {}: {}", file, why),
+            }
+        }
+      }
+      Err(why) => println!("[ERR] {}", why),
     }
-    let mut file = String::new();
-    file.push_str("feeds/");
-    file.push_str(self.streams[index].name.as_str());
-    file.push_str(".rss");
-    match fs::remove_file(file.as_str()) {
-      Ok(_) => {
-        println!("Removed {}", self.streams[index].name);
-        self.streams.remove(index);
-      },
-      Err(why) => println!("[ERROR] Can't remove {}: {}", file, why),
+  }
+
+  fn output_news(&mut self) {
+    let mut buffer = String::new();
+    for elem in self.streams.iter() {
+      for item in elem.get_unread_articles() {
+        match item.title {
+          Some(ref v) => {
+            print!("{clear}{goto}", clear = termion::clear::All , goto = termion::cursor::Goto(1,1));
+            print!("{}",v);
+            match item.content {
+              Some(ref v) => {
+                println!("  ==> {}", v);
+                io::stdin().read_line(&mut buffer).unwrap();
+              }
+              None => {
+                match item.description {
+                  Some(ref v) => {
+                    println!(" ==> {}", v);
+                    io::stdin().read_line(&mut buffer).unwrap();
+                  },
+                  None => {}
+                }
+              }
+            }
+          },
+          None => {}
+        }
+      }
     }
   }
 }
